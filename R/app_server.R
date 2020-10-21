c1f760beecabd131962c85eeb7ce796ffb9891c1
Lise Vaudor <lvaudor@cchumuser02.in2p3.fr>
2020-10-21 13:09:50 +0200

    add current date to app.R

--- R/app_server.R
+++ R/app_server.R
@@ -1,7 +1,12 @@
 #' @import shiny
 app_server <- function(input, output,session) {
   # List the first level callModules here
-  
+  onStop(function() {
+    # 1. File name
+    p <- paste0(getwd(), "/app.R")
+    # 2. Update file 'date creation'
+    Sys.setFileTime(p, Sys.time())
+  }) # onStop
   callModule(mod_long_profiles_server, "long_profiles_ui_1")
   callModule(mod_regional_models_server, "regional_models_ui_1")
 }
