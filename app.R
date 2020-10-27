3f9626035e9311a7dba73ceb93d1f2a5629243ba
Lise Vaudor <lvaudor@cchumuser02.in2p3.fr>
2020-10-27 16:28:59 +0100

    change project options

--- app.R
+++ app.R
@@ -1,11 +1,13 @@
-# Launch the ShinyApp (Do not remove this comment)
-# To deploy, run: rsconnect::deployApp()
-# Or use the blue button on top of this file
+golem::detach_all_attached()
 
-pkgload::load_all()
-options( "golem.app.prod" = TRUE)
 mylibraries=c(.libPaths(),
               "../../../R/x86_64-pc-linux-gnu-library/3.5")
+library(shiny,lib.loc=mylibraries)
+library(mapdO,lib.loc=mylibraries)
 library(vctrs,lib.loc=mylibraries)
 library(rlang,lib.loc=mylibraries)
-mapdO::run_app() # add parameters here (if any)
+library(dplyr,lib.loc=mylibraries)
+library(sf, lib.loc=mylibraries)
+shinyApp(ui=mapdO::app_ui,server=mapdO::app_server) # add parameters here (if any)# Launch the ShinyApp (Do not remove this comment)
+# To deploy, run: rsconnect::deployApp()
+# Or use the blue button on top of this file
