9686ee9032961a30cd8bc968b931139c43cbc461
Lise Vaudor <lvaudor@cchumuser02.in2p3.fr>
2020-10-27 16:37:46 +0100

    add function get_datapath

--- R/fct_get_long_data.R
+++ R/fct_get_long_data.R
@@ -1,20 +1,39 @@
+#' Get datapath towards file with metric data for a river
+#'
+#' @param axis river id
+#' @param y metric
+#'
+#' @return path towards metric data for river
+#' @export 
+#'
+#' @examples
+#' get_datapath(1,"Profil en long (m)")
+get_datapath=function(axis,y){
+  if(!is.null(y)){
+    tibinput=table_profiles %>% 
+      dplyr::filter(type==y)
+    filename=tibinput %>%
+      dplyr::pull(filename)
+    if(length(filename)==0){stop("There is no datafile for this.")}
+    ncpath=glue::glue("data-raw/axes/axis{axis}/{filename}.nc")
+    return(ncpath)
+  }
+}
+
 #' Get data regarding metric for a river
 #'
 #' @param axis river id
 #' @param y metric
 #'
-#' @return
+#' @return tibble with data xvar and yvar
 #' @export
 #'
 #' @examples
-#' get_data(1,"Long profile(m)")
+#' get_data(1,"Profil en long (m)")
 get_data=function(axis,y){
-  if(!is.null(y)){
-  tibinput=table_profiles %>% 
-    dplyr::filter(type==y)
-  filename=tibinput %>%
-    dplyr::pull(filename)
-  ncpath=glue::glue("{dat.path}/axis{axis}/{filename}.nc")
+ncpath=get_datapath(axis,y)
+tibinput=table_profiles %>% 
+  dplyr::filter(type==y)
   datanc=tidync::tidync(ncpath)%>% 
     tidync::hyper_tibble() %>% 
     dplyr::select(dplyr::everything(),
@@ -22,5 +41,4 @@ get_data=function(axis,y){
                   yvar=dplyr::matches(tibinput %>% dplyr::pull(yname))) %>% 
     dplyr::arrange(xvar)
   return(datanc)
-  }
 }
