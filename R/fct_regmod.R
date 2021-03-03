9239a530bd15e213994f59dce742e523ce63cafd
Lise Vaudor <lvaudor@cchumworker02.in2p3.fr>
2021-03-03 15:57:45 +0100

    regmod_labels

--- R/fct_regmod.R
+++ R/fct_regmod.R
@@ -25,7 +25,7 @@ calc_regmod=function(data,x,y){
     dplyr::mutate(residuals=model$residuals) %>% 
     dplyr::mutate(relpos=dplyr::min_rank(residuals)) %>% 
     dplyr::mutate(relpos=relpos/max(relpos))
-  return(list(data=data,model=model))
+  return(list(data=data,model=model, labels=c(x,y)))
 }
 
 
@@ -35,13 +35,13 @@ calc_regmod=function(data,x,y){
 #' @export
 #' @examples
 #' plot_regmod(calc_regmod(datRMC %>% sf::st_drop_geometry(),
-#' "surface de la bande fluviale",
-#' "surface de la bande active"))
+#'  "surface de la bande fluviale",
+#'  "surface de la bande active"))
 plot_regmod=function(data_mod){
   ggplot2::ggplot(data_mod$data, ggplot2::aes(x=xvar,y=yvar))+
     ggplot2::geom_point(ggplot2::aes(color=relpos))+
     ggplot2::geom_smooth(method="lm")+
-    ggplot2::labs(x=x,y=y)+
+    ggplot2::labs(x=data_mod$labels[1],y=data_mod$labels[2])+
     ggplot2::scale_color_gradient2(low="green",mid="yellow",high="red",midpoint=0.5)
 }
 
